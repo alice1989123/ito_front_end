491f4f9eae45f15fd313888c53dd8541e64961ad
alicie1989123 <aliciabasilo.ab@gmail.com>
2022-07-07 22:37:28 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,26 +4,27 @@ import "./App.css";
 import { ethers } from "ethers";
 import { useForm } from "react-hook-form";
 
-const api = `http://localhost:3001/`;
+const api =
+  //"https://74zkcr4wuj.execute-api.eu-central-1.amazonaws.com/default/";
+  `http://localhost:3001/`;
 
-const Bearer =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI2NmUzNDA5OS04ZmI5LTRjMjQtOWUwOS1jOWI3YzNhNWE5MGIiLCJlbWFpbCI6ImFsaWNpYWJhc2lsby5hYkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlfSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiOTgzY2RjNmQyNTI2MjQyY2RiODIiLCJzY29wZWRLZXlTZWNyZXQiOiIyNjA5NjFhMDRhMWNiYWI2ZTI0ZWE2NjYyNjZkOGY5ZjA5YzljZTQ4MDZhZTZiY2YxNThmMDJlNWMxY2Q5YmU0IiwiaWF0IjoxNjQxODY0MTM2fQ.UZ63Opzr-c7gFXlsucOERPDmibh7MCjojKhOgvwGazQ";
-
-const Upload =
-  "https://74zkcr4wuj.execute-api.eu-central-1.amazonaws.com/default/UploAdToPinata";
-const gateWay = "https://gateway.pinata.cloud/ipfs/";
+const gateWay = "https://infura-ipfs.io/ipfs/";
 
 function App() {
   const [ipfs, setIPFS] = useState(null);
-  const [imageURL, setImageURL] = useState(null);
   const [file, setFile] = useState();
   const [inputList, setInputList] = useState([]);
+  const [metadata, setmetadata] = useState(null);
+  const [metadataValues, setMetadataValues] = useState(null);
+  const [contract, setContract] = useState(null);
 
   const [Owner, setOwner] = useState(false);
   const [account, setAccount] = useState("");
   const [NFTsupply, setNFTsupply] = useState("");
   const [loading, setLoading] = useState(false);
   const [connectionError, setConnectionError] = useState("");
+  const [client, setClient] = useState(null);
+  const [artist, setArtist] = useState(null);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
     SCAN_LINK: "",
@@ -43,33 +44,85 @@ function App() {
     MAX_MINTING_VALUE: 0,
   });
 
-  const mintNFT = async (client, metadata) => {
-    var configPinata = {
-      method: "post",
-      url: "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+  const contracts = [
+    {
+      artist: "Andres",
+      address: "0x9282396A80076D8f5a2FC3744b510D99BB524b1b",
+      contract: "0xd3304ae95F09B605a8B1888cC995C13f436491f3",
+    },
+  ];
+
+  const connect = async () => {
+    const abiResponse = await fetch("/abi.json", {
       headers: {
-        Authorization: `Bearer ${Bearer}`,
-        "content-type": "application/json",
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
-      data: metadata,
-    };
+    });
+    const abi = await abiResponse.json();
 
-    //data.pinataOptions = { wrapWithDirectory: true };
+    const interface_ = new ethers.utils.Interface(abi);
 
+    const metamaskIsInstalled = window.ethereum && window.ethereum.isMetaMask;
+    if (metamaskIsInstalled) {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await provider.send("eth_requestAccounts", []);
+        console.log(
+          contracts.filter((x) => {
+            console.log(x.address.toLowerCase() == accounts[0].toLowerCase());
+            return x.address.toLowerCase() == accounts[0].toLowerCase();
+          })
+        );
+        const selectedAcounts = contracts.filter((x) => {
+          return x.address.toLowerCase() == accounts[0].toLowerCase();
+        });
+        if (selectedAcounts.length >= 1) {
+          //console.log(selectedAcounts[0]);
+          setArtist(selectedAcounts[0]);
+          setContract(selectedAcounts[0].contract);
+        }
+
+        const signer = await provider.getSigner();
+
+        const contract = new ethers.Contract(contract, interface_, signer);
+        const supply = await contract.totalSupply();
+        const owner = await contract.owner();
+        setAccount(accounts[0]);
+
+        setNFTsupply(parseInt(supply.toString()));
+        if (owner.toLowerCase() == accounts[0].toLowerCase()) {
+          setOwner(true);
+        }
+      } catch (e) {
+        console.log("error ", e);
+      }
+    } else {
+      setConnectionError(" Metmask is not installed, please install metamask");
+    }
+  };
+
+  const registerMetadata = async (metadata_) => {
     var req = {
       method: "post",
       url: `${api}registerMetadata`,
       headers: {
-        "content-type": "application/json",
+        "Content-Type": "application/json",
       },
-      data: metadata,
+      data: metadata_,
     };
-    const metadatahash = await axios(req);
-    console.log(metadatahash);
-    //const metadatahash = await axios(config);
-    //console.log(metadatahash);
-    //console.log(metadatahash);
-    const metadataURL = `ipfs://${metadatahash.data}`;
+    const hash = await axios(req);
+    //metadata = await axios.get(gateWay + hash.data);
+
+    setmetadata(hash.data);
+
+    console.log(hash, "metadata");
+    return hash.data;
+  };
+
+  const mintNFT = async (client) => {
+    console.log(metadata);
+    const metadataURL = `ipfs://${metadata}`;
     let cost = CONFIG.WEI_COST;
     let gasLimit = CONFIG.GAS_LIMIT;
     let NFTsValue = ethers.utils.parseEther(String(CONFIG.DISPLAY_COST));
@@ -104,7 +157,7 @@ function App() {
 
           const signer = await provider.getSigner();
           const Mintcontract1 = new ethers.Contract(
-            CONFIG.CONTRACT_ADDRESS,
+            contract,
             new ethers.utils.Interface(abi),
             signer
           );
@@ -118,7 +171,7 @@ function App() {
 
             setNFTsupply(parseInt(supply.toString()));
             const Mintcontract2 = new ethers.Contract(
-              CONFIG.CONTRACT_ADDRESS,
+              contract,
               new ethers.utils.Interface(abi),
               signer
             );
@@ -163,14 +216,14 @@ function App() {
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => {
-    const metadata = {
+    const metadata_ = {
       description: `${data.description}`,
       external_url: "https://www.itolabs.io/",
       image: `ipfs://${ipfs}`,
       name: `${data.name}`,
       attributes: inputList,
     };
-    mintNFT(data.clientAddress, metadata);
+    registerMetadata(metadata_);
   };
 
   //console.log(watch("example"))
@@ -190,18 +243,36 @@ function App() {
     getConfig();
   }, []);
 
+  const getMetadataValues = async () => {
+    try {
+      const values = await axios.get(`${gateWay}` + metadata);
+      console.log(values.data);
+      setMetadataValues(values.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getMetadataValues();
+  }, [metadata]);
+
   function handleChange(event) {
     setFile(event.target.files[0]);
   }
 
-  async function removeInput(index) {
+  function handleChangeClient(event) {
+    setClient(event.target.value);
+  }
+
+  function removeInput(index) {
     setInputList([...inputList.slice(0, index), ...inputList.slice(index + 1)]);
   }
-  async function addInput() {
+  function addInput() {
     setInputList([...inputList, {}]);
   }
 
-  async function HandleKeyChange(event, index) {
+  function HandleKeyChange(event, index) {
     setInputList([
       ...inputList.slice(0, index),
       { trait_type: event.target.value, value: inputList[index].value },
@@ -209,7 +280,7 @@ function App() {
     ]);
   }
 
-  async function HandleValueChange(event, index) {
+  function HandleValueChange(event, index) {
     setInputList([
       ...inputList.slice(0, index),
       { trait_type: inputList[index].trait_type, value: event.target.value },
@@ -228,36 +299,99 @@ function App() {
       '{"cidVersion": 0, "wrapWithDirectory": "false"}'
     );
 
-    var config = {
+    var config_ = {
       method: "post",
-      url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+      url: api + "upLoadImage",
       headers: {
-        Authorization: `Bearer ${Bearer}`,
         "content-type": "multipart/form-data",
       },
       data: formData,
     };
 
-    const res = await axios(config);
-    setIPFS(res.data.IpfsHash);
-    console.log(res.data);
-    setImageURL(gateWay + ipfs);
+    const res = await axios(config_);
+    console.log(res);
+
+    setIPFS(res.data);
   }
+
+  function handleMint(e) {
+    e.preventDefault();
+    console.log(client);
+    mintNFT(client);
+  }
+
+  const Preview = (metadata) => {
+    return (
+      <div>
+        {metadataValues && (
+          <>
+            <h3> Preview </h3>
+            <h4> Name </h4>
+            <div>{metadataValues?.name}</div>
+            <h4> Description </h4>
+            <div>{metadataValues?.description}</div>
+            <h4> Image </h4>{" "}
+            {metadataValues?.image && (
+              <img
+                width={400}
+                src={gateWay + metadataValues?.image.split("ipfs://")[1]}
+              ></img>
+            )}
+            <h4> Attributes </h4>
+            {metadataValues?.attributes?.map((x) => (
+              <div>
+                <div>
+                  {" "}
+                  {x?.trait_type} : {x?.value}{" "}
+                </div>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    );
+  };
+
+  const LoggIn = () => {
+    return (
+      <div>
+        {artist && (
+          <div className={{ display: "flex" }}>
+            <h4>Artist </h4>
+            <div>{artist?.artist}</div>
+            <h4>Account </h4>
+            <div>{artist?.address}</div>
+            <h4>Contract </h4>
+            <div>{artist?.contract}</div>
+          </div>
+        )}
+      </div>
+    );
+  };
 
   return (
     <div className="App">
+      <LoggIn> </LoggIn>
+      <button onClick={() => connect()}>Connect Wallet</button>
       <form onSubmit={handleSubmitImage}>
         <h1>Ito tatoo App</h1>
+        <h3> Upload image</h3>
+
         <input type="file" onChange={handleChange} />
         <button type="submit">Upload</button>
       </form>
-      {imageURL && <img width={400} src={imageURL}></img>}
+      {ipfs && <img width={400} src={gateWay + ipfs}></img>}
       <div className="App">
-        <h3> Atributes</h3>
-        {inputList.map((x, i) => {
-          return (
-            <div className="box">
-              <form>
+        <h3> Upload metadata</h3>
+
+        <form className={{ display: "flex" }} onSubmit={handleSubmit(onSubmit)}>
+          Name <input {...register("name", { required: true })} />
+          Description
+          <input {...register("description", { required: true })} />
+          {errors.exampleRequired && <span>This field is required</span>}
+          {inputList.map((x, i) => {
+            return (
+              <div className="box">
                 <input
                   onChange={(e) => {
                     HandleKeyChange(e, i);
@@ -278,24 +412,21 @@ function App() {
                 >
                   Remove
                 </button>
-              </form>
-            </div>
-          );
-        })}
-        <button onClick={addInput}>Add Atribute</button>
-        {/*         <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
-         */}
-        <form className={{ display: "flex" }} onSubmit={handleSubmit(onSubmit)}>
-          Name <input {...register("name", { required: true })} />
-          Description
-          <input {...register("description", { required: true })} />
-          Client Address
-          <input {...register("clientAddress", { required: true })} />
-          {errors.exampleRequired && <span>This field is required</span>}
+              </div>
+            );
+          })}
+          <h4> Add aditional atributes</h4>
+          <button onClick={addInput}>Add Atribute</button>
           <input type="submit" />
-        </form>{" "}
+        </form>
+
+        <Preview metadata={metadata} />
       </div>
-      <div>{JSON.stringify(inputList)}</div>
+      <h4> Mint NFT</h4>
+      Client Address
+      <input onChange={(e) => handleChangeClient(e)} />
+      <button onClick={(e) => handleMint(e)}> Mint </button>
+      <div>{client}</div>
     </div>
   );
 }
